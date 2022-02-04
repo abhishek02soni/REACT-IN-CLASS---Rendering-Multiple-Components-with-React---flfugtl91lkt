7679f7982a26899a642b4d4d2f00162e689cdc4b
abhishek02soni <92570611+abhishek02soni@users.noreply.github.com>
2022-02-04 17:01:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import Name from "./Name";
+import Des from "./Des";
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <Name  data-ns-test ="project-name"/>
+     <Des data-ns-test ="project-description"/>
+    </div>
   )
 }
 
