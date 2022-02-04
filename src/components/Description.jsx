61a3ee97c21603021f9dc0e1123006b38077e61f
abhishek02soni <92570611+abhishek02soni@users.noreply.github.com>
2022-02-04 16:05:21 +0530

    Create Description.jsx

---
+++ src/components/Description.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Description extends React.Component{
+  render(){
+    return(
+      <h3>This is my Describtion</h3>
+    )
+  }
+}
+export default Description;
