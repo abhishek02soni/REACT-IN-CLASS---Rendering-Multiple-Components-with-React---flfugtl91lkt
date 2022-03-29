2ac1a8748286e2d91fbe0fb29ae09298c62b5cf2
abhishek02soni <92570611+abhishek02soni@users.noreply.github.com>
2022-03-29 23:00:20 +0530

    Update Name.jsx

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -2,7 +2,7 @@ import React from "react";
 class Name extends React.Component{
   render(){
     return(
-      <h1>This is Abhishek Soni</h1>
+      <h1 data-ns-test ="project-name">This is Abhishek Soni</h1>
     )
   }
 }
